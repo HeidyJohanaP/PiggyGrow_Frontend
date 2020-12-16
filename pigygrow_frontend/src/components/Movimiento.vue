68253bbe1d81ea258051a19310933b2479929874
Heidy Johana P <64294135+HeidyJohanaP@users.noreply.github.com>
2020-12-15 20:42:00 -0500

    Update Movimiento.vue

--- pigygrow_frontend/src/components/Movimiento.vue
+++ pigygrow_frontend/src/components/Movimiento.vue
@@ -1,13 +1,8 @@
 <template>
-<<<<<<< HEAD
   <div id="Movimiento">
     <h2>NUEVO MOVIMIENTO</h2>
     <br /><br />
-    <div>
-        <label>id:{{id}}</label><br /><br />
-=======
-    <div id="Movimiento">
-        <h2>NUEVO MOVIMIENTO</h2><br><br>
+    <div><label>id:{{id}}</label><br /><br /> </div>    
         <label>Tipo:  </label>
         <select v-model="tipo" id="tipo" name="TipoMov">
             <option selected value="">Elige una opción</option>
@@ -23,45 +18,16 @@
         <label>Fecha:  </label>
         <input v-model="fecha" id="fecha" type="date"><br><br>
         <button v-on:click = "NewMov" >Agregar</button>
-
->>>>>>> f2e3e1429b39b4d6b8f52e6163bc952a1107aec8
     </div>
-    <label>Tipo: </label>
-    <select v-model="tipo" id="tipo" name="TipoMov">
-      <option selected value="">Elige una opción</option>
-      <option value="Ingreso">Ingreso</option>
-      <option value="Gasto">Gasto</option></select
-    ><br /><br />
-    <label>Valor: </label>
-    <input v-model="valor" id="valor" type="numeric" /><br /><br />
-    <label>Categoria: </label>
-    <input v-model="categoria" id="categoria" type="text" /><br /><br />
-    <label>Descripción: </label>
-    <input
-      v-model="descripcion"
-      id="descripcion"
-      type="text"
-      style="height: 50px"
-    /><br /><br />
-    <label>Fecha: </label>
-    <input v-model="fecha" id="fecha" type="date" /><br /><br />
-    <button v-on:click="NewMov">Agregar</button>
-  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-   
-
     name: "crear_movimiento",
     data: function() {
         return {
-<<<<<<< HEAD
-            id: 0,
-=======
             id: 10,
->>>>>>> f2e3e1429b39b4d6b8f52e6163bc952a1107aec8
             tipo: "",
             fecha: "",
             valor: "",
